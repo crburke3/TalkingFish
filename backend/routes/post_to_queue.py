3d9ea54b21aa34eb1041745315a9e8e68d77584e
Allen Keimig <akeimig@bandwidth.com>
2021-11-16 11:48:45 -0500

    Actually push this time

--- backend/routes/post_to_queue.py
+++ backend/routes/post_to_queue.py
@@ -5,7 +5,7 @@ from structs import server_globals
 def post_to_queue(request):
     print("parameters passed: ", request)
     json_data = request.json()
-    message = request['message']
+    message = json_data['message']
     text = message['text']
     server_globals.fish_firestore.add_request_to_queue(text)
     return json.dumps({"text_added_to_queue": text}), 201
