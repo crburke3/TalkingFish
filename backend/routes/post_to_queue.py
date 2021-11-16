56bc3a98aee175964ba0ff853e2b71773bed33b6
Allen Keimig <akeimig@bandwidth.com>
2021-11-16 14:47:57 -0500

    Allow for parsing of bandwidth callback

--- backend/routes/post_to_queue.py
+++ backend/routes/post_to_queue.py
@@ -7,7 +7,7 @@ def post_to_queue(request):
     print("parameters passed: ", request)
     request_json = request.get_json()
     print("Json body: ", request_json)
-    text = request.get_json()['message']['text']
+    text = request.get_json()[0]['message']['text']
     commands = TextToCommands.convertTextToCommands(text)
     server_globals.fish_firestore.add_request_to_queue(text, commands)
     return json.dumps({"text_added_to_queue": text, "commands": commands}), 201
--- backend/tests/post_to_queue_test.py
+++ backend/tests/post_to_queue_test.py
@@ -3,11 +3,24 @@ from _local_resources_.TestClass import TestClass
 
 
 def test_bubbles_object_creation_success_201():
-    fake_post = TestClass({
-        "message": {
-            "text": "Hello, world!!"
+    fake_post = TestClass([
+        {'time': '2021-11-16T16:35:57.159Z', 
+         'type': 'message-received', 
+         'to': '+19843586100', 
+         'description': 'Incoming message received', 
+         'message': {
+             'id': '891b9238-c8b7-43e3-b681-56e74c482d57', 
+             'owner': '+19843586100', 
+             'applicationId': 'f50bebeb-57a7-4dda-bbb4-3567292acbff', 
+             'time': '2021-11-16T16:35:57.062Z', 
+             'segmentCount': 1, 
+             'direction': 'in', 
+             'to': ['+19843586100'], 
+             'from': '+17036095909', 
+             'text': 'Hello World'
+             }
         }
-    })
+    ])
     fake_post.path = "post_to_queue"
     fake_post.method = "POST"
     main.hello_world(fake_post)
