eaa716470798d1d06add204f04788e52d9defc62
Allen Keimig <akeimig@bandwidth.com>
2021-11-16 11:59:26 -0500

    It's actually working locally now

--- backend/routes/post_to_queue.py
+++ backend/routes/post_to_queue.py
@@ -4,8 +4,6 @@ from structs import server_globals
 
 def post_to_queue(request):
     print("parameters passed: ", request)
-    json_data = request.json()
-    message = json_data['message']
-    text = message['text']
+    text = request.get_json()['message']['text']
     server_globals.fish_firestore.add_request_to_queue(text)
     return json.dumps({"text_added_to_queue": text}), 201
