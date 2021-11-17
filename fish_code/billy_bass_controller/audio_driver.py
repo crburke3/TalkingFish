e3efb1946451254b392f7006984fd147d0309f60
crburke3 <crburke3@ncsu.edu>
2021-11-16 22:51:29 -0500

    deletes existing WAV fail

--- fish_code/billy_bass_controller/audio_driver.py
+++ fish_code/billy_bass_controller/audio_driver.py
@@ -1,13 +1,14 @@
-import pygame
+import pygame, wave
 import os
-
 
 class AudioDriver:
 
-    def __init__(self):
-        pygame.mixer.init()
+    # def __init__(self):
 
     def play_wav_file(self, file_path: str):
+        file_wav = wave.open(file_path)
+        frequency = file_wav.getframerate()
+        pygame.mixer.init(frequency=frequency)
         assert os.path.exists(file_path)
         pygame.mixer.music.load(file_path)
         pygame.mixer.music.play()
