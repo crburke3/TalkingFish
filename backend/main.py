7814dd2c4cf7bedd9022454f1a5e25dffb3638e8
Allen Keimig <akeimig@bandwidth.com>
2021-11-16 23:36:00 -0500

    remove test code

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ import json
 from routes.post_to_queue import post_to_queue
 from routes.get_from_queue import get_from_queue
 from structs import server_globals, fish_firestore
-from _local_resources_.TestClass import TestClass
 
 # Globals Creation
 server_globals.fish_firestore = fish_firestore.FishFirestore()  # initalize when main runs.
@@ -24,10 +23,3 @@ def hello_world(request):
         return get_from_queue()
     else:
         return json.dumps({"ERROR": "unknown route"}), 401
-
-
-if __name__ == '__main__':
-    fake_get = TestClass({})
-    fake_get.path = "get_from_queue"
-    fake_get.method = "GET"
-    hello_world(fake_get)
