705ccbdb061cb1c6fc43ea52f70171fc6e7d212e
crburke3 <crburke3@ncsu.edu>
2021-11-16 23:43:55 -0500

    updated motor test

--- fish_code/_tests/motor_test.py
+++ fish_code/_tests/motor_test.py
@@ -4,6 +4,7 @@ sys.path.insert(1, '../')
 
 # user defined modules
 from billy_bass_controller import Device
+from billy_bass_controller import FishCommand
 
 device = Device()
 
@@ -23,3 +24,8 @@ time.sleep(1)
 
 
 # try and run a series of commands
+fake_command = FishCommand()
+fake_command.commands = ['C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'C2', 'O4', 'C2', 'O4', 'C2', 'O1',
+                                 'C2', 'O1', 'C2', 'O4', 'C2', 'O1', 'C2', 'O4', 'C2', 'C2']
+device.fc.current_task = fake_command
+device.fc._move_to_commands()
