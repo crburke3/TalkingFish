66f8d9e7645f14a597e5a47666084568b6921559
crburke3 <crburke3@ncsu.edu>
2021-11-17 13:08:54 -0500

    updated setup script

--- fish_code/_tools/raspberry_pi/setup.sh
+++ fish_code/_tools/raspberry_pi/setup.sh
@@ -1,1 +1,18 @@
-pip3 install -r requirements.txt
+sudo chmod a+x /home/pi/TalkingFish/fish_code/_tools/raspberry_pi/boot.sh
+echo "add the following line to the end of the next file: "
+echo ""
+echo "@reboot bash /home/pi/TalkingFish/fish_code/_tools/raspberry_pi/boot.sh"
+echo ""
+echo ""
+sleep 2
+
+echo "Press any key and the ENTER to continue"
+while [ true ] ; do
+read -t 3 -n 1
+if [ $? = 0 ] ; then
+exit ;
+else
+echo "waiting for the keypress/ENTER"
+fi
+sudo crontab -e
+done
