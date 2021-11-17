ddf538952b49bad8f4e581b6b3694a0c368691c8
crburke3 <crburke3@ncsu.edu>
2021-11-17 00:09:24 -0500

    updated performance test

--- fish_code/_tests/performance_test_1.py
+++ fish_code/_tests/performance_test_1.py
@@ -7,16 +7,7 @@ from billy_bass_controller import Device
 from billy_bass_controller import FishCommand
 
 
-device = Device()
 
-fake_command = FishCommand()
-fake_command.commands = ['C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'C2', 'O4', 'C2', 'O4', 'C2', 'O1',
-                                 'C2', 'O1', 'C2', 'O4', 'C2', 'O1', 'C2', 'O4', 'C2', 'C2']
-fake_command.song_url = "https://storage.googleapis.com/fish-1-audio-files/joke.wav"
-fake_command.speech_text = "You know where I keep my money? The river bank"
-device.fish_api.download_song_for_object(fake_command)
-
-device.fc.current_task = fake_command
 # device.fc.perform(fake_command)
 
 def run_parallel(*functions):
@@ -32,5 +23,19 @@ def run_parallel(*functions):
     for proc in processes:
         proc.join()
 
+
 if __name__ == '__main__':
+    device = Device()
+
+    fake_command = FishCommand()
+    fake_command.commands = ['C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'C2', 'O4', 'C2', 'O4',
+                             'C2', 'O1',
+                             'C2', 'O1', 'C2', 'O4', 'C2', 'O1', 'C2', 'O4', 'C2', 'C2']
+    fake_command.song_url = "https://storage.googleapis.com/fish-1-audio-files/joke.wav"
+    fake_command.speech_text = "You know where I keep my money? The river bank"
+    device.fish_api.download_song_for_object(fake_command)
+
+    device.fc.current_task = fake_command
+
+    print("BEGINNING PARALLELISM")
     run_parallel(device.fc._move_to_commands())
