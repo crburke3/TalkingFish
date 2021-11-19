2d09428437b9890aff2cce3272e56a9bea2f3fd7
crburke3 <crburke3@ncsu.edu>
2021-11-19 15:00:20 -0500

    updated boot scripts

--- fish_code/_tools/raspberry_pi/boot.sh
+++ fish_code/_tools/raspberry_pi/boot.sh
@@ -8,5 +8,7 @@ git stash
 git pull --force
 echo "installing dependencies..."
 pip3 install -r _tools/raspberry_pi/requirements.txt
+
+# DO NOT FUCK WITH tHE STUFF UP TOP
 echo "starting fish!"
 python3 main.py
