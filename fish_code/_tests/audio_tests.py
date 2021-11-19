509a557a2e17597673d68539e66ebd0ba84177f0
crburke3 <crburke3@ncsu.edu>
2021-11-18 23:07:02 -0500

    new test

--- fish_code/_tests/audio_tests.py
+++ fish_code/_tests/audio_tests.py
@@ -2,12 +2,12 @@ from billy_bass_controller.audio_driver import AudioDriver
 
 
 def test_mp3():
-    file_path = "../_resources/man-scream-01.mp3"
+    file_path = "../_resources/scream.mp3"
     driver = AudioDriver()
     driver.play_file(file_path)
 
 
 def test_get_mp3_length():
-    file_path = "../_resources/man-scream-01.mp3"
+    file_path = "../_resources/scream.mp3"
     driver = AudioDriver()
     print(driver.get_audio_length_seconds(file_path))
--- fish_code/_tests/performance_test_1.py
+++ fish_code/_tests/performance_test_1.py
@@ -15,10 +15,8 @@ if __name__ == '__main__':
     device = Device()
 
     fake_command = FishCommand()
-    fake_command.commands = ['C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'C2', 'O4', 'C2', 'O4',
-                             'C2', 'O1',
-                             'C2', 'O1', 'C2', 'O4', 'C2', 'O1', 'C2', 'O4', 'C2', 'C2']
-    fake_command.song_url = "https://storage.googleapis.com/fish-1-audio-files/joke.wav"
+    fake_command.commands = ['O:4', 'C:2', 'C:2', 'O:4', 'C:2', 'O:1', 'O:4', 'C:2', 'O:4', 'C:2', 'O:4', 'C:2', 'O:3', 'C:2', 'C:2', 'O:1', 'C:2', 'C:2', 'O:4', 'C:2', 'C:2', 'C:2', 'O:1', 'C:2', 'O:4', 'C:2', 'O:1']
+    fake_command.song_url = "https://storage.googleapis.com/fish-1-audio-files/audio_1637294327.mp3"
     fake_command.speech_text = "You know where I keep my money? The river bank"
     device.fish_api.download_song_for_object(fake_command)
 
