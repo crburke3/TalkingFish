e439fe75ca05405f27d8b5e8e13066ce64299348
crburke3 <crburke3@ncsu.edu>
2021-11-18 23:24:33 -0500

    fixed performance test

--- fish_code/_tests/wav_test.py
+++ fish_code/_tests/wav_test.py
@@ -11,17 +11,17 @@ from billy_bass_controller import FishCommand
 # device.fc.perform(fake_command)
 
 
-# if __name__ == '__main__':
-device = Device()
+if __name__ == '__main__':
+    device = Device()
 
-fake_command = FishCommand()
-fake_command.commands = ['C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'C2', 'O4', 'C2', 'O4',
-                             'C2', 'O1',
-                             'C2', 'O1', 'C2', 'O4', 'C2', 'O1', 'C2', 'O4', 'C2', 'C2']
-fake_command.song_url = "https://storage.googleapis.com/fish-1-audio-files/joke.wav"
-fake_command.speech_text = "You know where I keep my money? The river bank"
-device.fish_api.download_song_for_object(fake_command)
+    fake_command = FishCommand()
+    fake_command.commands = ['C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'O4', 'C2', 'C2', 'O4', 'C2', 'O4',
+                                 'C2', 'O1',
+                                 'C2', 'O1', 'C2', 'O4', 'C2', 'O1', 'C2', 'O4', 'C2', 'C2']
+    fake_command.song_url = "https://storage.googleapis.com/fish-1-audio-files/joke.wav"
+    fake_command.speech_text = "You know where I keep my money? The river bank"
+    device.fish_api.download_song_for_object(fake_command)
 
-fake_command._expected_prescaler = 0.04789855072463768
-# fake_command.audio_start_offset = 0.5
-device.fc.perform(fake_command)
+    fake_command._expected_prescaler = 0.04789855072463768
+    # fake_command.audio_start_offset = 0.5
+    device.fc.perform(fake_command)
