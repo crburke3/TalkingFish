9018ded3df0538e167f7654504719b075f9c5d84
crburke3 <crburke3@ncsu.edu>
2021-11-19 14:56:08 -0500

    updated boot scripts

--- fish_code/_tools/raspberry_pi/boot.sh
+++ fish_code/_tools/raspberry_pi/boot.sh
@@ -1,16 +1,16 @@
 echo "Sleeping for 30 seconds to allow boot"
 sleep 30
-#echo "moving to fish_code dir"
-#cd /home/pi/TalkingFish/fish_code
-#pwd
-#echo "pulling latest code..."
-#git stash
-#git pull --force
-#echo "installing dependencies..."
-#pip3 install -r _tools/raspberry_pi/requirements.txt
-#echo "starting fish!"
-#python3 main.py
-#sleep 10
-#sudo pkill python3
-#sleep 2
+echo "moving to fish_code dir"
+cd /home/pi/TalkingFish/fish_code
+pwd
+echo "pulling latest code..."
+git stash
+git pull --force
+echo "installing dependencies..."
+pip3 install -r _tools/raspberry_pi/requirements.txt
+echo "starting fish!"
+python3 main.py
+sleep 10
+sudo pkill python3
+sleep 2
 lxterminal -e python3 /home/pi/TalkingFish/fish_code/main.py
