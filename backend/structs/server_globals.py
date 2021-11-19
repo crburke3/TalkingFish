10c82071ec30f53aa2ae2979e05765e8fcb7e58f
Allen Keimig <akeimig@bandwidth.com>
2021-11-18 23:35:19 -0500

    Store audio as a wav. again...

--- backend/structs/server_globals.py
+++ backend/structs/server_globals.py
@@ -4,11 +4,11 @@ import os
 # User defined modules
 from structs.fish_firestore import FishFirestore
 from structs.text_to_commands import TextToCommands
-from structs.mp3_creator import Mp3Creator
+from structs.wav_creator import WavCreator
 
 base_folder_path = f"{os.path.dirname(os.path.abspath(__file__))}".replace("/structs", "")
 gcs_cred_path = f"{base_folder_path}/resources/credentials.json"
 
 fish_firestore: FishFirestore = None
 text_to_commands: TextToCommands = None
-mp3_creator: Mp3Creator = None
+wav_creator: WavCreator = None
