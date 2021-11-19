2e6c454962393c11e8b1a9de6d5d9ec0e622d3d5
crburke3 <crburke3@ncsu.edu>
2021-11-18 23:35:59 -0500

    fixed performance test

--- fish_code/_tests/audio_tests.py
+++ fish_code/_tests/audio_tests.py
@@ -11,3 +11,8 @@ def test_get_mp3_length():
     file_path = "../_resources/scream.mp3"
     driver = AudioDriver()
     print(driver.get_audio_length_seconds(file_path))
+
+def test_wav_play():
+    file_path = "../_resources/joke.wav"
+    driver = AudioDriver()
+    driver.play_file(file_path)
