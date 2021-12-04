ed89648f8916c75cbe07cdc8f1d6cb2b2908138b
crburke3 <crburke3@ncsu.edu>
2021-12-04 00:18:53 -0500

    added motor controller back

--- fish_code/billy_bass_controller/device.py
+++ fish_code/billy_bass_controller/device.py
@@ -14,7 +14,7 @@ class Device:
         self.fish_api: FishAPI
         self._initalize_motor_controller()
         self._initalize_audio_driver()
-        # self._initalize_fish_controller()
+        self._initalize_fish_controller()
         # self._initalize_fish_api()
 
     def _initalize_motor_controller(self):
