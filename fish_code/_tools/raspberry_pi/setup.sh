57c3953d2b72cb181d4491302d52090c66f93a95
crburke3 <crburke3@ncsu.edu>
2021-12-04 00:25:57 -0500

    can actually use the setup.sh now

--- fish_code/_tools/raspberry_pi/setup.sh
+++ fish_code/_tools/raspberry_pi/setup.sh
@@ -1,3 +1,7 @@
+echo "Please go through all the prompts on this next command. And reboot 1st time. Do not reboot 2nd time!"
+sleep 3
+curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh | bash
+
 sudo chmod a+x /home/pi/TalkingFish/fish_code/_tools/raspberry_pi/boot.sh
 sudo chmod 777 /home/pi/TalkingFish/fish_code/billy_bass_controller/downloads
 sudo apt-get install python-pygame
