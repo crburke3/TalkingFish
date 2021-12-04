7fba9f76dcf835e224161693402312edf8f737a8
crburke3 <crburke3@ncsu.edu>
2021-12-03 23:55:27 -0500

    fucjk pygame

--- fish_code/billy_bass_controller/audio_driver.py
+++ fish_code/billy_bass_controller/audio_driver.py
@@ -1,4 +1,6 @@
-import pygame as pg
+# import pygame as pg
+import os
+
 import audioread
 from pydub import AudioSegment
 
@@ -10,26 +12,29 @@ vol = 0.75
 
 class AudioDriver:
 
-    def __init__(self, volume=vol):
-        pg.mixer.init(freq, bitsize, channels, buffer)
-        pg.mixer.music.set_volume(volume)
+    # def __init__(self, volume=vol):
+    #     pg.mixer.init(freq, bitsize, channels, buffer)
+    #     pg.mixer.music.set_volume(volume)
 
-    def play_file(self, file_path: str):
-        """
-        stream music with mixer.music module in blocking manner
-        this will stream the sound from disk while playing
-        """
-        clock = pg.time.Clock()
-        try:
-            pg.mixer.music.load(file_path)
-            print("Music file {} loaded!".format(file_path))
-        except pg.error:
-            print("File {} not found! {}".format(file_path, pg.get_error()))
-            return
+    def play_file(self):
+        os.system("aplay /usr/share/sounds/alsa/Front_Center.wav &")
 
-        pg.mixer.music.play()
-        while pg.mixer.music.get_busy():
-            clock.tick(30)
+    # def play_file(self, file_path: str):
+    #     """
+    #     stream music with mixer.music module in blocking manner
+    #     this will stream the sound from disk while playing
+    #     """
+    #     clock = pg.time.Clock()
+    #     try:
+    #         pg.mixer.music.load(file_path)
+    #         print("Music file {} loaded!".format(file_path))
+    #     except pg.error:
+    #         print("File {} not found! {}".format(file_path, pg.get_error()))
+    #         return
+    #
+    #     pg.mixer.music.play()
+    #     while pg.mixer.music.get_busy():
+    #         clock.tick(30)
 
     def get_audio_length_seconds(self, file_path: str) -> float:
         if not file_path:
