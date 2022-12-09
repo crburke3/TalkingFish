4c94d3dedaf72d53d15bd15cfb59f472dbcce98b
crburke3 <crburke3@ncsu.edu>
2022-12-09 00:35:28 -0600

    sped up performace

--- fish_code/billy_bass_controller/fish_command.py
+++ fish_code/billy_bass_controller/fish_command.py
@@ -20,15 +20,19 @@ class FishCommand:
         audio_diver = AudioDriver()
         if not self._expected_prescaler:
             if not self.local_song_url:
+                print("Using default prescalar: 0.25")
                 self._expected_prescaler = 0.25
                 return 0.25
+            print("Calculating prescalar...")
             total_units = self.command_unit_length()
             song_time_s = audio_diver.get_audio_length_seconds(self.local_song_url)
             if total_units == 0:
                 total_units = 1
             expected_prescalar = float(song_time_s / total_units) - self.audio_start_offset
+            self._expected_prescaler = expected_prescalar
             return expected_prescalar
         else:
+            print("Prescalar already cached")
             return self._expected_prescaler
 
     def command_unit_length(self) -> int:
