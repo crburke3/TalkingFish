2adc3a4d5937533bf6152339a25fe224541483c8
crburke3 <crburke3@ncsu.edu>
2022-12-08 22:23:40 -0600

    parses form from twillio

--- backend/tests/post_fish_data_test.py
+++ backend/tests/post_fish_data_test.py
@@ -4,8 +4,15 @@ from _local_resources_.TestClass import TestClass
 
 
 def test_bubbles_object_get_success_200():
-    fake_post = TestClass({"device_id": "THIS A TEST"})
-    fake_post.path = "add_fish_data"
+    post_data = [
+        {
+            "message": {
+                "text": "be careful. it might be fishy down there"
+            }
+        }
+    ]
+    fake_post = TestClass(post_data)
+    fake_post.path = "post_to_queue"
     fake_post.method = "POST"
     resp = main.hello_world(fake_post)
-    assert resp[1] == 200
+    assert resp[1] == 201
