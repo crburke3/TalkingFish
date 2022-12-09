25a475b074b425b4d4bc9d4e7cdc65a72adfbafe
crburke3 <crburke3@ncsu.edu>
2022-12-08 23:54:29 -0600

    added mac bootup

--- fish_code/billy_bass_controller/ports/raspberry_pi/raspberry_pi_motor_controller.py
+++ fish_code/billy_bass_controller/ports/raspberry_pi/raspberry_pi_motor_controller.py
@@ -1,7 +1,7 @@
 # system modules
 from gpiozero import LED
 import time
-import time
+from datetime import datetime
 
 # user defined modules
 from billy_bass_controller import MotorController
@@ -22,45 +22,45 @@ class RPIMotorController(MotorController):
             pin.off()
 
     def turn_on_upper_body(self):
-        start_time = time.time()
+        start_time = datetime.now().timestamp()
         assert not self.lower_body_on
         self.INA_1.on()
         self.upper_body_on = True
-        execution_time = time.time() - start_time
+        execution_time = datetime.now().timestamp() - start_time
         print(f"it took {execution_time}ms to turn ON upper body")
 
     def turn_off_upper_body(self):
-        start_time = time.time()
+        start_time = datetime.now().timestamp()
         self.INA_1.off()
         self.upper_body_on = False
-        execution_time = time.time() - start_time
+        execution_time = datetime.now().timestamp() - start_time
         print(f"it took {execution_time}ms to turn OFF upper body")
 
     def turn_on_lower_body(self):
-        start_time = time.time()
+        start_time = datetime.now().timestamp()
         assert not self.upper_body_on
         self.INA_2.on()
         self.lower_body_on = True
-        execution_time = time.time() - start_time
+        execution_time = datetime.now().timestamp() - start_time
         print(f"it took {execution_time}ms to turn ON lower body")
 
     def turn_off_lower_body(self):
-        start_time = time.time()
+        start_time = datetime.now().timestamp()
         self.INA_2.off()
         self.lower_body_on = False
-        execution_time = time.time() - start_time
+        execution_time = datetime.now().timestamp() - start_time
         print(f"it took {execution_time}ms to turn OFF lower body")
 
     def turn_on_mouth(self):
-        start_time = time.time()
+        start_time = datetime.now().timestamp()
         self.INA_3.on()
-        execution_time = time.time() - start_time
+        execution_time = datetime.now().timestamp() - start_time
         print(f"it took {execution_time}ms to turn ON mouth")
 
     def turn_off_mouth(self):
-        start_time = time.time()
+        start_time = datetime.now().timestamp()
         self.INA_3.off()
-        execution_time = time.time() - start_time
+        execution_time = datetime.now().timestamp() - start_time
         print(f"it took {execution_time}ms to turn off mouth")
 
 
