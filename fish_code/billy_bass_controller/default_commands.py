e9b9d22a794a2d8f09a32b045af406b9dd7ccf83
crburke3 <crburke3@ncsu.edu>
2022-12-09 02:39:00 -0600

    fixing dfown wiht sickness

--- fish_code/billy_bass_controller/default_commands.py
+++ fish_code/billy_bass_controller/default_commands.py
@@ -71,7 +71,7 @@ def down_with_the_sickness():
                         "LOWER_ON:0.75s", "LOWER_OFF:0.75s", "LOWER_ON:0.75s", "LOWER_OFF:0.75s",
                         "SYNC:@7.1",
                         "UPPER_ON:0.0s",
-                        "O:0.25s", "C:0.1s", "O:0.5s", "C:0",  # O WAH AH AH AH
+                        "O:0.25s", "C:0.1s", "O:0.65s", "C:0",  # O WAH AH AH AH
                         "LOWER_ON:0.75s", "LOWER_OFF:0.75s", "LOWER_ON:0.75s", "LOWER_OFF:0.75s",
                         ]
     command.local_song_url = f"{base_path}/../_resources/down_with_the_sickness.wav"
