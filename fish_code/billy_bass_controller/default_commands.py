2f3759eb2313c8e9baf5440d6926dbb250b1d560
crburke3 <crburke3@ncsu.edu>
2022-12-08 23:19:58 -0600

    added mac bootup

--- fish_code/billy_bass_controller/default_commands.py
+++ fish_code/billy_bass_controller/default_commands.py
@@ -14,5 +14,5 @@ def mac_boot():
     command = FishCommand()
     command.commands = ["LOWER_ON:2", "O:2", "LOWER_OFF:2", "C:2", "UPPER_ON:2", "UPPER_OFF:2", "LOWER_ON:2", "LOWER_OFF:2",
                         "LOWER_ON:2", "LOWER_OFF:2", "O:2"]
-    command.local_song_url = f"{base_path}/../_resources/joke.wav"
+    command.local_song_url = f"{base_path}/../_resources/mac_startup_sound.mp3"
     return command
