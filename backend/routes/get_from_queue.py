5f3cf2067dd2de66b2021fcf5d263621b80ee857
crburke3 <crburke3@ncsu.edu>
2022-12-09 17:06:33 -0600

    local file?

--- backend/routes/get_from_queue.py
+++ backend/routes/get_from_queue.py
@@ -14,13 +14,12 @@ def get_from_queue(request):
     if bubbles == 404:
         print("There is nothing on the queue. Returning a 404...")
         return json.dumps({}), 404
-    try:
-        if 'commands' in bubbles:
-            commands = bubbles['commands']
-        if 'audio_url' in bubbles:
-            audio_url = bubbles['audio_url']
-
-        server_globals.fish_firestore.delete_request_from_queue(bubbles['queue_count'], device_id)
-        return json.dumps({"commands": commands, "audio_url": audio_url}), 200
-    except:
+    if 'commands' in bubbles:
+        commands = bubbles['commands']
+    if 'audio_url' in bubbles:
+        audio_url = bubbles['audio_url']
+    if "local_file" in bubbles:
         return json.dumps(bubbles), 200
+
+    server_globals.fish_firestore.delete_request_from_queue(bubbles['queue_count'], device_id)
+    return json.dumps({"commands": commands, "audio_url": audio_url}), 200
