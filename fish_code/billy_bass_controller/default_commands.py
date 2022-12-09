8ff965e66d2776a5d5364b193c94fed2b51ae82a
crburke3 <crburke3@ncsu.edu>
2022-12-09 02:54:44 -0600

    fixing dfown wiht sickness

--- fish_code/billy_bass_controller/default_commands.py
+++ fish_code/billy_bass_controller/default_commands.py
@@ -71,12 +71,12 @@ def down_with_the_sickness():
                         "LOWER_ON:0.75s", "LOWER_OFF:0.75s", "LOWER_ON:0.75s", "LOWER_OFF:0.75s",
                         "SYNC:@6.5",
                         "UPPER_ON:0.0s", "O:0.25s", "C:0.1s", "O:0.65s", "C:0.1s", "UPPER_OFF:0.5s",   # O WAH AH AH AH
-                        "SYNC:@9.5",
-                        "UPPER_ON:0.0s", "O:0.5s", "C:0.1s", "O:0.4s", "C:0.1s", "UPPER_OFF:0.5s",  # UH UH
-                        "SYNC:@12.2",
-                        "UPPER_ON:0.0s", "O:0.5s", "C:0.1s", "O:0.4s", "C:0.1s", "UPPER_OFF:0.5s",  # UH UH
+                        "SYNC:@9.4",
+                        "UPPER_ON:0.1s", "O:0.5s", "C:0.1s", "O:0.4s", "C:0.1s", "UPPER_OFF:0.5s",  # UH UH
+                        "SYNC:@12.1",
+                        "UPPER_ON:0.1s", "O:0.5s", "C:0.1s", "O:0.4s", "C:0.1s", "UPPER_OFF:0.5s",  # UH UH
                         "SYNC:@14.5",
-                        "UPPER_ON:0.0s", "O:0.5s", "C:0.1s", "O:0.4s", "C:0.1s", "UPPER_OFF:0.5s",  # UH UH
+                        "UPPER_ON:0.1s", "O:0.5s", "C:0.1s", "O:0.4s", "C:0.1s", "UPPER_OFF:0.5s",  # UH UH
     ]
     command.local_song_url = f"{base_path}/../_resources/down_with_the_sickness.wav"
     return command
