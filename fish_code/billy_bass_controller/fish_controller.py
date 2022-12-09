1f56b2f60c701b292852bde37fa6565649681723
crburke3 <crburke3@ncsu.edu>
2022-12-08 23:40:30 -0600

    added mac bootup

--- fish_code/billy_bass_controller/fish_controller.py
+++ fish_code/billy_bass_controller/fish_controller.py
@@ -149,9 +149,9 @@ class FishController:
         prescaler = self.current_task.get_expected_prescaler()
         sleep_time = units * prescaler
         print("sleeping for (s): ", sleep_time)
-        sleep_start = datetime.now()
+        sleep_start = datetime.now().timestamp()
         time.sleep(sleep_time)
-        sleep_time = datetime.now() - sleep_start
+        sleep_time = datetime.now().timestamp() - sleep_start
         print(f"it took {sleep_time}s to sleep")
 
     def _toggle_body(self):
