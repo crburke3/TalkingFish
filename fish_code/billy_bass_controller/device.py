48b3750135145275fd2f18df5f86f4b8709c1a56
crburke3 <crburke3@ncsu.edu>
2022-12-09 12:07:28 -0600

    look at me now

--- fish_code/billy_bass_controller/device.py
+++ fish_code/billy_bass_controller/device.py
@@ -69,6 +69,7 @@ class Device:
         GPIO.setwarnings(False)  # Ignore warning for now
         # GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
         GPIO.setup(RPI_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set
+        print(f"PIN SETUP WITH GPIO: {RPI_BUTTON_PIN}")
 
     def get_unique_id(self) -> str:
         mac_raw = getnode()
